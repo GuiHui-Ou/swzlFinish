abba5fadfb3bb25c98840f8786cc279128b017ba
leo <ouguihui@126.com>
2021-09-14 11:29:58 +0800

    数据库语句

--- src/main/java/com/ou/swzl/mapper/UserMapper.java
+++ src/main/java/com/ou/swzl/mapper/UserMapper.java
@@ -19,4 +19,14 @@ public interface UserMapper {
     int userRegister(UserRequest request);
 
 
+    /**
+     * 数据库语句
+     */
+//    CREATE TABLE `user` (
+//            `user_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+//            `user_name` varchar(225) NOT NULL DEFAULT '' COMMENT '用户名',
+//            `user_password` varchar(200) NOT NULL COMMENT '用户密码',
+//            `user_phone_number` char(13) NOT NULL DEFAULT '',
+//    PRIMARY KEY (`user_id`)
+//) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8
 }
